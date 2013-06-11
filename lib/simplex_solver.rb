bda55683f48c31dc5178044f5d8ac722b1a81fd2
Tim Felgentreff <timfelgentreff@gmail.com>
2013-06-11 14:58:39 +0200

    we need to optimize when adding an edit constraint

--- lib/simplex_solver.rb
+++ lib/simplex_solver.rb
@@ -23,7 +23,7 @@ module Cassowary
       unless try_adding_directly(expr)
         add_with_artificial_variable(expr)
       end
-      if auto_solve
+      if auto_solve or constraint.edit_constraint?
         optimize(objective)
         set_external_variables
       end
@@ -137,7 +137,7 @@ module Cassowary
         prev_edit_constants.delete_at(index)
       end
 
-      if auto_solve
+      if auto_solve or constraint.edit_constraint?
         optimize(zrow)
         set_external_variables
       end
